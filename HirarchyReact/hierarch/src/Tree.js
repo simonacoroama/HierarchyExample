86549557b9fca2e5c3aa121b7611701aba9a9177
Simona Coroama <simona.coroama@centric.eu>
2022-09-06 00:51:54 +0300

    Add products

--- HirarchyReact/hierarch/src/Tree.js
+++ HirarchyReact/hierarch/src/Tree.js
@@ -4,20 +4,32 @@ import Tree from 'react-d3-tree';
 import { useState, useEffect } from "react";
 
 export default function OrgChartTree() {
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch(`http://localhost:5198/Hierarchy`)
-        .then((response) => response.json())
+        .then((response) => {
+          return response.json();
+        })
         .then((actualData) => {
-            setProducts(actualData)
+             return setProducts(actualData);
     })
+    .finally(() => {
+      return setLoading(false);
+  });
        }, []);
+
+  if(loading){
+    return (<div>loading...</div>);
+  }
 
     return (
       // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
+      <div style={{margin:'1em'}}>
       <div id="treeWrapper" style={{ width: '500em', height: '200em' }}>
-        <Tree data={products} orientation="vertical" initialDepth={1}/>
+        <Tree data={products} orientation="vertical" initialDepth={0} style={{margin:'1em'}}/>
+      </div>
       </div>
     );
   }
--- HirarchyReact/hierarch/src/index.js
+++ HirarchyReact/hierarch/src/index.js
@@ -8,7 +8,7 @@ import OrgChartTree from './Tree';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
    <OrgChartTree />
   </React.StrictMode>
 );
