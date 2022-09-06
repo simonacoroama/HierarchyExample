26111f953bde4a3485826e7558c747621f1b0a68
Cristiana <cristiana.grigoriu@centric.eu>
2022-09-06 11:11:59 +0300

    Add project for tree with cards

---
+++ Tree/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Tree from "./CenteredTree";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Tree />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
